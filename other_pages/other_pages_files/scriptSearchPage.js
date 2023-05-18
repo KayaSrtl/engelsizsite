48b088a0051150a52781d9ffda072fe59d769f52
Kaya Sertel <50380412+KayaSrtl@users.noreply.github.com>
2023-05-18 13:11:52 +0300

    Update scriptSearchPage.js

--- other_pages/other_pages_files/scriptSearchPage.js
+++ other_pages/other_pages_files/scriptSearchPage.js
@@ -4,10 +4,10 @@ var is_mobile_phone = ( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Oper
 var selected_book_index = 1;
 
 var last_book_index = 10;
-var search_header_text;
+var search_header_text = [];
 
-var Turkish_codes = ["%C3%A7", "%C3%87", "%C3%B6", "%C3%96", "%C5%9F", "%C5%9E", "%C4%B1", "%C4%B0", "%C4%9F", "%C4%9E", "%C3%BC", "%C3%9C"];
-var Turkish_chars = ["ç", "Ç", "ö", "Ö", "ş", "Ş", "ı", "İ", "ğ", "Ğ", "ü", "Ü"];
+var Turkish_codes = ["%C3%A7", "%C3%87", "%C3%B6", "%C3%96", "%C5%9F", "%C5%9E", "%C4%B1", "%C4%B0", "%C4%9F", "%C4%9E", "%C3%BC", "%C3%9C", "%27", "%3C", "%3E", "%C2%A3", "%C2%BD", "%C3%A9"];
+var Turkish_chars = ["ç", "Ç", "ö", "Ö", "ş", "Ş", "ı", "İ", "ğ", "Ğ", "ü", "Ü", "'", "<", ">", "£", "½", "é"];
 
 
 //import data from '../panel/panelsayfasi/data.json' assert { type: 'json' };
@@ -15,6 +15,14 @@ var Turkish_chars = ["ç", "Ç", "ö", "Ö", "ş", "Ş", "ı", "İ", "ğ", "Ğ",
 
 //../panel/panelsayfasi/data.json
 
+
+var stringsToSearch = [
+  "JavaScript Basics",
+  "Advanced JavaScript Techniques",
+  "Introduction to JavaScript",
+  "JavaScript for Web Development",
+  "JavaScript Frameworks andweb Libraries"
+];
 
 
 
@@ -32,33 +40,66 @@ var Turkish_chars = ["ç", "Ç", "ö", "Ö", "ş", "Ş", "ı", "İ", "ğ", "Ğ",
 	var itemcounter = 0, is_pickable = false;
 	index = 0;
 	console.log(myObj[1].name);
+	var strings_for_search = [];
+	var strings_for_search_temp;
+	
+	
 	for(var i = 0; i < objlen; i++) {
-		
-			if(search_header_text == myObj[i].name) {
-				itemcounter++;
-				$('#search_result_' + (itemcounter - 1)).children( ".search_result_div" ).children( ".literature_name" ).text(myObj[i].name);
-				$('#search_result_' + (itemcounter - 1)).children( ".search_result_div" ).children( ".literature_writer" ).text(myObj[i].writer);
-				$('#search_result_' + (itemcounter - 1)).children( ".search_result_div" ).children( ".literature_language" ).text(myObj[i].language);
-				$('#search_result_' + (itemcounter - 1)).children( ".search_result_div" ).children( ".literature_format" ).text(myObj[i].format);
-				$('#search_result_' + (itemcounter - 1)).children( ".search_result_div" ).children( ".literature_subject" ).text(myObj[i].subject);
-				$('#search_result_' + (itemcounter - 1)).children( ".search_result_div" ).children( ".literature_link" ).text(myObj[i].link);
-				$('#search_result_' + (itemcounter - 1)).children( ".search_result_div" ).children( ".literature_link" ).attr("href", myObj[i].link);
-				
-				if(itemcounter > 1) {
-					$( ".search_result_div_outer" ).clone().appendTo( ".result_show_inner" ).prop('id', 'search_result_' + itemcounter);
-				}
-			}
-		if(itemcounter == 1) {
-			$(".search_result_div_outer").css('display', 'flex');
-			$(".book_not_found_txt").css('display', 'none');
-		}
+		strings_for_search_temp = myObj[i].name;
+		strings_for_search_temp += myObj[i].writer;
+		strings_for_search_temp += myObj[i].language;
+		strings_for_search_temp += myObj[i].format;
+		strings_for_search_temp += myObj[i].subject;
+		strings_for_search.push(strings_for_search_temp);
 	}
+	
+	
+	var searchResultsIndex = searchString(search_header_text, strings_for_search);
+	/*console.log("Search Results:");
+	for (var i = 0; i < searchResultsIndex.length; i++) {
+		console.log(searchResultsIndex[i]);
+	}*/
+	
+	
+	for(var i = 0; i < searchResultsIndex.length; i++) {
+		if(i >= 1) {
+			$( ".search_result_div_outer" ).clone().appendTo( ".result_show_inner" ).prop('id', 'search_result_' + i);
+		}
+		$('#search_result_' + i).children( ".search_result_div" ).children( ".literature_name" ).text(myObj[searchResultsIndex[i]].name);
+		$('#search_result_' + i).children( ".search_result_div" ).children( ".literature_writer" ).text(myObj[searchResultsIndex[i]].writer);
+		$('#search_result_' + i).children( ".search_result_div" ).children( ".literature_language" ).text(myObj[searchResultsIndex[i]].language);
+		$('#search_result_' + i).children( ".search_result_div" ).children( ".literature_format" ).text(myObj[searchResultsIndex[i]].format);
+		$('#search_result_' + i).children( ".search_result_div" ).children( ".literature_subject" ).text(myObj[searchResultsIndex[i]].subject);
+		$('#search_result_' + i).children( ".search_result_div" ).children( ".literature_link" ).text(myObj[searchResultsIndex[i]].link);
+		$('#search_result_' + i).children( ".search_result_div" ).children( ".literature_link" ).attr("href", myObj[searchResultsIndex[i]].link);
+		
+		
+	}
+	
+	if(searchResultsIndex.length) {
+		$(".search_result_div_outer").css('display', 'flex');
+		$(".book_not_found_txt").css('display', 'none');
+	} else {
+		$(".book_not_found_txt").text("Aranan Kitap Bulunamadı!");
+	}
+	
 	
   })
   .catch(error => {
     // Handle any errors that occur during the fetch request
     console.log('Error:', error);
   });
+
+
+function searchString(searchTerm, strings) {
+  var results = [];
+  for (var i = 0; i < strings.length; i++) {
+    if (strings[i].toLowerCase().includes(searchTerm.toLowerCase())) {
+      results.push(i);
+    }
+  }
+  return results;
+}
 
 
 
@@ -74,28 +115,24 @@ $( document ).ready(function() {
 		
 	
 	index += 8;
-	if(url[index] != "%") {
-		search_header_text = url[index];
-		index++;
-	} else {
-		for(var i = 0; i < Turkish_codes.length; i++) {
-				if(url.slice(index, index+6) == Turkish_codes[i]) {
-					search_header_text = Turkish_chars[i];
-					index += 6;
-					break;
-				}
-			}
-	}
+	console.log(url);
+	
 	while(index < url.length) {
 		if(url[index] == "%") {
+			var isout = false;
 			for(var i = 0; i < Turkish_codes.length; i++) {
 				//console.log(url.slice(index, index+6) + " " + Turkish_codes[i]);
-				if(url.slice(index, index+6) == Turkish_codes[i]) {
+				if(url.slice(index, index+Turkish_codes[i].length) == Turkish_codes[i]) {
 					search_header_text += Turkish_chars[i];
-					index += 6;
+					index += Turkish_codes[i].length;
+					isout = true;
 					break;
 				}
+				
 			}
+			
+			if(!isout)
+					index++;
 		} else if (url[index] == "+") {
 			search_header_text += " ";
 			index++;
